24400c160c55e2dbee637768e05714d2b59038fb
renato <88345498+renatobicego@users.noreply.github.com>
2023-12-30 14:40:11 -0300

    Update layout.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     title: "Mimpronta - Consultora de Impacto",
     description: 'Transformamos habilidades en propósitos e intenciones en acciones, ayudando a generar un impacto social positivo.',
     images: [{
-      url: 'https://firebasestorage.googleapis.com/v0/b/mimpronta-6fa99.appspot.com/o/logo.png?alt=media&token=0ffc813c-1469-4852-8259-874279cb6d11'
+      url: 'https://firebasestorage.googleapis.com/v0/b/mimpronta-6fa99.appspot.com/o/weeeb.jpg?alt=media&token=150c556a-b34b-453c-ba21-3c9fef9d7291'
     }]
   }
 }
