391f77ae934aca49d2d05430d32833e16a1a63c3
renatobicego <renatobicego@gmail.com>
2024-01-08 19:19:10 -0300

    Json ld data

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,66 +1,93 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Montserrat, Montserrat_Alternates } from 'next/font/google'
-import Header from './components/Header/Header'
-import { Suspense } from 'react'
-import Loading from './loading'
-import Footer from './components/Footer/Footer'
-import { Providers } from './providers'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import "./globals.css";
+import type { Metadata } from "next";
+import { Montserrat, Montserrat_Alternates } from "next/font/google";
+import Header from "./components/Header/Header";
+import { Suspense } from "react";
+import Loading from "./loading";
+import Footer from "./components/Footer/Footer";
+import { Providers } from "./providers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const montserrat = Montserrat({
-  weight: ['400', '500', '600', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-mont',
-})
+  weight: ["400", "500", "600", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-mont",
+});
 
 const montserratAlt = Montserrat_Alternates({
-  weight: ['400', '500', '600', '700', '800'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-montAlter',
-})
+  weight: ["400", "500", "600", "700", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montAlter",
+});
 
 export const metadata: Metadata = {
-  title: 'mimpronta',
-  description: 'Ayudamos a combinar habilidades con las necesidades del mundo.',
-  keywords: ['Next.js', 'Mimpronta', 'ambiental', 'impacto', 'consciencia', 'empresa'],
+  title: "Mimpronta",
+  description: "Ayudamos a combinar habilidades con las necesidades del mundo.",
+  keywords: [
+    "Next.js",
+    "Mimpronta",
+    "ambiental",
+    "impacto",
+    "consciencia",
+    "empresa",
+  ],
   openGraph: {
     title: "MIMPRONTA - Formación y Consultoría de impacto",
-    description: 'Ayudamos a combinar habilidades con las necesidades del mundo.',
-    images: 'https://firebasestorage.googleapis.com/v0/b/mimpronta-6fa99.appspot.com/o/weeeb.jpg?alt=media&token=150c556a-b34b-453c-ba21-3c9fef9d7291'
+    description:
+      "Ayudamos a combinar habilidades con las necesidades del mundo.",
+    images:
+      "https://firebasestorage.googleapis.com/v0/b/mimpronta-6fa99.appspot.com/o/weeeb.jpg?alt=media&token=150c556a-b34b-453c-ba21-3c9fef9d7291",
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     title: "MIMPRONTA - Formación y Consultoría de impacto",
-    description: 'Ayudamos a combinar habilidades con las necesidades del mundo.',
-    images: ['https://firebasestorage.googleapis.com/v0/b/mimpronta-6fa99.appspot.com/o/weeeb.jpg?alt=media&token=150c556a-b34b-453c-ba21-3c9fef9d7291'], // Must be an absolute URL
+    description:
+      "Ayudamos a combinar habilidades con las necesidades del mundo.",
+    images: [
+      "https://firebasestorage.googleapis.com/v0/b/mimpronta-6fa99.appspot.com/o/weeeb.jpg?alt=media&token=150c556a-b34b-453c-ba21-3c9fef9d7291",
+    ], // Must be an absolute URL
   },
   robots: {
     index: true,
     follow: true,
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "NGO",
+    name: "Mimpronta",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/mimpronta-6fa99.appspot.com/o/weeeb.jpg?alt=media&token=150c556a-b34b-453c-ba21-3c9fef9d7291",
+    description:
+      "Ayudamos a combinar habilidades con las necesidades del mundo.",
+  };
   return (
-    <html lang="en" className='!overflow-x-hidden scroll-smooth'>
-      <body className={`${montserrat.variable} ${montserratAlt.variable} bg-white`}>
+    <html lang="en" className="!overflow-x-hidden scroll-smooth">
+      <body
+        className={`${montserrat.variable} ${montserratAlt.variable} bg-white`}
+      >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
         <Suspense fallback={<Loading />}>
           <Providers>
             <Header />
-              {children}
+            {children}
             <Footer />
           </Providers>
         </Suspense>
         <SpeedInsights />
       </body>
     </html>
-  )
+  );
 }
