3e2c8c7429ad738f868514bb3c39d777fe56cf27
renatobicego <renatobicego@gmail.com>
2024-05-09 12:52:30 -0300

    Fix delete comment

--- client/src/app/blog/[title]/CommentCard.tsx
+++ client/src/app/blog/[title]/CommentCard.tsx
@@ -8,6 +8,7 @@ import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.share
 
 const CommentCard = ({ comment, router }: { comment: Comment; router: AppRouterInstance }) => {
   const { isAdmin } = useAdmin();
+  console.log(isAdmin)
   const handleDeleteComment = async () => {
     Swal.fire({
       text: "¿Está seguro/a que desea borrar el comentario?",
@@ -36,18 +37,19 @@ const CommentCard = ({ comment, router }: { comment: Comment; router: AppRouterI
   };
 
   return (
-    <div className="text-xs md:text-sm shadow-sm border rounded-2xl w-full py-4 px-5 flex justify-between gap-3">
-      <p>
+    <div className="text-xs md:text-sm shadow-sm border rounded-2xl w-full py-4 px-5 flex items-center justify-between gap-3">
+      <p className="flex-1">
         <span className="text-azul font-semibold">{comment.name}</span>:{" "}
         {comment.text}
       </p>
       {isAdmin && (
-        <button onClick={handleDeleteComment}>
+        <button className="w-6 h-6" onClick={handleDeleteComment}>
           <Image
             alt="borrar comentario"
             src={"/icons/deleteComment.png"}
             width={20}
             height={20}
+            className="w-full h-full"
           />
         </button>
       )}
