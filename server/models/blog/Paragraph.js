23a4777e187f53a30ceec729f1d759dbd66af4e4
renatobicego <renatobicego@gmail.com>
2025-12-15 21:12:15 -0300

    validation

--- server/models/blog/Paragraph.js
+++ server/models/blog/Paragraph.js
@@ -1,20 +1,19 @@
 const { Schema, model } = require("mongoose");
 
-
 const ParagraphSchema = Schema({
-    text: {
-        type: String,
-        required: [true, 'Texto obligatorio']
-    },
-    imgParagraph: {
-        type: Schema.Types.ObjectId,
-        ref: 'Image',
-        default: null
-    },
-    subtitle: {
-        type: String,
-        default: ""
-    }
-})
+  text: {
+    type: String,
+    default: "",
+  },
+  imgParagraph: {
+    type: Schema.Types.ObjectId,
+    ref: "Image",
+    default: null,
+  },
+  subtitle: {
+    type: String,
+    default: "",
+  },
+});
 
-module.exports = model('Paragraph', ParagraphSchema)
+module.exports = model("Paragraph", ParagraphSchema);
