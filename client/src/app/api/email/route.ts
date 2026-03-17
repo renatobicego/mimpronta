8e564da0b0e325b92276da0a37bd8e148df871c0
renatobicego <renatobicego@gmail.com>
2026-03-17 20:50:44 -0300

    change email provider

--- client/src/app/api/email/route.ts
+++ client/src/app/api/email/route.ts
@@ -1,33 +1,26 @@
 import { type NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
 
-import sgMail from "@sendgrid/mail";
-
-sgMail.setApiKey(process.env.SEND_GRID_KEY as string);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
   const { email, name, country, message } = await request.json();
-  
-  const msg = {
-    to: "micaelagerbeno@mimpronta.com",
-    from: "renatobicego@mimpronta.com",
-    subject: "Solicitud de contacto: " + name,
-    dynamicTemplateData: {
-      message,
-      name,
-      country,
-      email
-    },
-    templateId: "d-d50bdd5d98674c61ab7e441ebfd649b5"
-  };
 
   try {
-    await sgMail.send(msg);
+    await resend.emails.send({
+      to: "micaelagerbeno@mimpronta.com",
+      from: "renatobicego@mimpronta.com",
+      subject: "Solicitud de contacto: " + name,
+      html: `
+        <p><strong>Nombre:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>País:</strong> ${country}</p>
+        <p><strong>Mensaje:</strong> ${message}</p>
+      `,
+    });
     return NextResponse.json({ message: "Email sent" });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
-
-
-
