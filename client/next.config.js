0e47ebf3c1cf083c1ad97829f372dd359e7eb6aa
renatobicego <renatobicego@gmail.com>
2026-05-14 12:52:35 -0300

    index

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
